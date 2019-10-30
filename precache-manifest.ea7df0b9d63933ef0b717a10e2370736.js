49d39c31fd35fe4869d5eba247674833bfe9b10a
amserhii11 <amelin.serhii@gmail.com>
2019-10-30 12:59:53 +0200

    Updates

---
+++ precache-manifest.ea7df0b9d63933ef0b717a10e2370736.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1caa9b5e7bac79c9a7f267c31e2e5036",
+    "url": "/shoptest/index.html"
+  },
+  {
+    "revision": "e5f1b8823f204588fd03",
+    "url": "/shoptest/static/css/main.1a2d1617.chunk.css"
+  },
+  {
+    "revision": "d1757eb4c80fd8e65f93",
+    "url": "/shoptest/static/js/2.70e2617e.chunk.js"
+  },
+  {
+    "revision": "e5f1b8823f204588fd03",
+    "url": "/shoptest/static/js/main.6b335fad.chunk.js"
+  },
+  {
+    "revision": "1e553e7eddcf6495bafb",
+    "url": "/shoptest/static/js/runtime-main.1e7de875.js"
+  }
+]);
